877e28c04fae2235a8bf032d79aa56827c9d2f13
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-12-29 10:10:28 -0300

    Adicionando Web Token ao projeto, adicionado a biblioteca Auth0 no projeto, criado a classe TokenService que vai gerenciar as configs relacionadas a token e dentro da classe TokenService criado os metodos gerarToken() para gerar o token através da biblioteca adicionada e o metodo para limitar o tempo de validade do token dataExpiracaoToken( )

---
+++ api-voll-med/api-voll-med/src/main/java/com/yagocloud/api_voll_med/intra/security/TokenService.java
@@ -0,0 +1,32 @@
+package com.yagocloud.api_voll_med.intra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.yagocloud.api_voll_med.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("aquï vai uma senha");
+            return JWT.create()
+                    .withIssuer("API Voll.med")//No geral aqui eu coloca a identificaçao da aplicacao
+                    .withSubject(usuario.getLogin())//Aqui eu estou armazenando quem é o usuário logado
+                    .withExpiresAt(dataExpiracaoToken( ))//Validade do token
+                    .sign(algoritmo);
+        } catch (JWTCreationException ex){
+            throw new RuntimeException("Error ao gerar token jwt", ex);
+        }
+    }
+    //Metodo para limitar o tempo que o token permanece valido
+    private Instant dataExpiracaoToken( ) {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3:00"));
+    }
+}
